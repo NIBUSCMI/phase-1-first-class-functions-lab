212fbaf22e2036d854cb9e360f230612f486485b
Nick <nbuscem1@binghamton.edu>
2022-05-26 19:27:04 -0400

    commit tree 84e65390993e954a4c061a55eb488e59a01b602a

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(firstDrivers){
+    return firstDrivers.slice(0,2);
+}
+console.log(returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']))
+
+const returnLastTwoDrivers = function(lastDrivers){
+    return lastDrivers.slice(-2);
+}
+console.log(returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']))
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+console.log(selectingDrivers)
+
+function createFareMultiplier(multiplier){
+    return function(fare) {
+        return fare * multiplier
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (arrayOfDrivers, driversToReturn){ 
+    return driversToReturn(arrayOfDrivers);
+
+}
+
+console.log(selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'], returnFirstTwoDrivers))
